8da6a54682eaf073053aaffb0a74773ed6a5e37f
OdearOgy <hovhan198@gmail.com>
2020-06-09 18:49:01 +0400

    Updates

---
+++ precache-manifest.ea834e23cec709029444c2707ea30614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b056f4e1066311160ffb5b8125ad2ee2",
+    "url": "/gh_trending_repos/index.html"
+  },
+  {
+    "revision": "9251df888911b82144db",
+    "url": "/gh_trending_repos/static/css/main.ac9a5277.chunk.css"
+  },
+  {
+    "revision": "287bd94c782b1b1bd03b",
+    "url": "/gh_trending_repos/static/js/2.0b06f66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh_trending_repos/static/js/2.0b06f66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9251df888911b82144db",
+    "url": "/gh_trending_repos/static/js/main.c86996b9.chunk.js"
+  },
+  {
+    "revision": "4321b4a4b0b6053cd855",
+    "url": "/gh_trending_repos/static/js/runtime-main.ef3804d9.js"
+  }
+]);
